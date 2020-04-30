0d9eaf1ff9392126e4f716e4d235b912fba46e8a
Rady2rady <rady2rady@yahoo.com>
2020-04-30 18:05:44 +0200

    aa

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
+// Third part
+
+$('h1').text('Done by JavaScript')
+$('p').css('border', '1px solid red');
+$('p').css('padding', '0px');
+$('h2').toggleClass('.blue');
+
 console.log('Script file is loaded.');
+
+// single line comment
+
+/*
+    Multiple 
+    lines 
+    of comments
+*/
+
+
